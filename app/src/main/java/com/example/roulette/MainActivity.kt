2900edd786a089ecb7a0da55703083dd30a36b35
yrgk <yarik090908@gmail.com>
2023-08-17 13:31:36 +0300

    finish

--- app/src/main/java/com/example/roulette/MainActivity.kt
+++ app/src/main/java/com/example/roulette/MainActivity.kt
@@ -3,44 +3,103 @@ package com.example.roulette
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.roulette.ui.theme.RouletteTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RouletteTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
+                var rotation by remember { mutableStateOf(0f) }
+
+                var number by remember { mutableStateOf("0") }
+
+                val angle by animateFloatAsState(
+                    targetValue = rotation,
+                    tween(durationMillis = 2000),
+                    finishedListener = {
+                        var index = ((360f - (it % 360)) / 10f).roundToInt()
+
+                        if (index == RouletteSectors.sectors.size ) {
+                            number = RouletteSectors.sectors[0]
+                        } else {
+                            number = RouletteSectors.sectors[index]
+                        }
+                    }
+                )
+
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                        //.background(Color(42, 115, 42)),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+
+                    
+                    Text(
+                        text = number,
+                        fontSize = 60.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.DarkGray,
+
+                        modifier = Modifier
+                            .padding(top = 30.dp, bottom = 50.dp)
+                    )
+
+
+                    Box(contentAlignment = Alignment.TopCenter,) {
+
+                        Image(
+                            painter = painterResource(id = R.drawable.roulette),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .padding(40.dp)
+                                .rotate(angle)
+                            )
+
+                        Image(
+                            painter = painterResource(id = R.drawable.arrow),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .padding(start = 150.dp, end = 150.dp)
+                        )
+
+                    }
+
+                    Button(
+                        onClick = { rotation = (720..1080).random().toFloat() + angle },
+                        modifier = Modifier.width(150.dp)//.height(0.dp)
+                    ) {
+                        Text(text = "Spin")
+                    }
+                    
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RouletteTheme {
-        Greeting("Android")
     }
 }
